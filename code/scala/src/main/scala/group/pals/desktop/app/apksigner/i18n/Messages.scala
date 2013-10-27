48e670d84e196d093b463576f0e5353cfdb80627
hai bison <hai bison@localhost>
2013-10-28 00:24:32 +0700

    Port Java to Scala (continue...)

---
+++ code/scala/src/main/scala/group/pals/desktop/app/apksigner/i18n/Messages.scala
@@ -0,0 +1,150 @@
+/*
+ *    Copyright (C) 2012 Hai Bison
+ *
+ *    See the file LICENSE at the root directory of this project for copying
+ *    permission.
+ */
+
+package group.pals.desktop.app.apksigner.i18n
+
+import group.pals.desktop.app.apksigner.utils.Preferences
+
+import java.beans.Beans
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
+import java.util.HashMap
+import java.util.Locale
+import java.util.Map
+import java.util.MissingResourceException
+import java.util.ResourceBundle
+
+import scala.collection.mutable.LinkedHashMap
+import scala.collection.mutable.Map
+
+/**
+ * Manager for i18n strings.
+ *
+ * @author Hai Bison
+ * @since v1.6 beta
+ */
+object Messages {
+
+    /**
+     * Default locale.
+     */
+    lazy final val DEFAULT_LOCALE = "en"
+
+    /**
+     * Map of available locale tags to their human readable names.
+     */
+    lazy final val AVAILABLE_LOCALES = LinkedHashMap[String, String]()
+
+    AVAILABLE_LOCALES += DEFAULT_LOCALE -> "English (Default)"
+    AVAILABLE_LOCALES += "vi" -> "Vietnamese (Tiếng Việt)"
+
+    private lazy final val BUNDLE_NAME = "group.pals.desktop.app.apksigner.i18n.messages" //$NON-NLS-1$
+    private lazy final val RESOURCE_BUNDLE = loadBundle()
+    private var mDefaultResourceBundle: ResourceBundle = null
+
+    /**
+     * Loads message resource.
+     *
+     * @return the resource bundle which contains message resource.
+     */
+    private def loadBundle(): ResourceBundle = {
+        var localeTag = Preferences.instance.localeTag
+        if (!AVAILABLE_LOCALES.contains(localeTag)) {
+            Preferences.instance.localeTag = DEFAULT_LOCALE
+            localeTag = DEFAULT_LOCALE
+        }
+        ResourceBundle.getBundle(BUNDLE_NAME, Locale.forLanguageTag(localeTag))
+    }// loadBundle()
+
+    /**
+     * Gets a string for current locale by its key. If it's not available for
+     * current locale, the default string in built-in locale (English) will
+     * return.
+     *
+     * @param key
+     *            the string's key.
+     * @return the string, or {@code null} if not found.
+     */
+    private def getString(key: String): String = {
+        try {
+            if (RESOURCE_BUNDLE.contains(key))
+                return RESOURCE_BUNDLE.get(key)
+
+            /*
+             * Try to find the `key` in default locale.
+             */
+            if (!new Locale(DEFAULT_LOCALE).getLanguage().equals(
+                    RESOURCE_BUNDLE.getLocale().getLanguage())) {
+                if (mDefaultResourceBundle == null)
+                    mDefaultResourceBundle = ResourceBundle.getBundle(
+                            BUNDLE_NAME, Locale.forLanguageTag(DEFAULT_LOCALE))
+                return mDefaultResourceBundle.get(key)
+            }
+
+            null
+        } catch {
+            case e: MissingResourceException => null
+        }
+    }// getString()
+
+    /**
+     * Map of resources IDs to their name.
+     */
+    private lazy final val MAP_IDS = Map[Int, String]()
+
+    /**
+     * Gets a string by its resource ID. If it's not available for current
+     * locale, the default string in built-in locale (English) will return.
+     *
+     * @param resId
+     *            the resource ID.
+     * @return the string, or {@code null} if not found.
+     */
+    def getString(resId: Int): String = {
+        if (MAP_IDS.contains(resId))
+            return getString(MAP_IDS.get(resId))
+
+        for (f <- R.string.getClass().getFields()) {
+            if (Modifier.isStatic(f.getModifiers())
+                    && f.getType().isAssignableFrom(Integer.TYPE)) {
+                try {
+                    if (f.getInt(null) == resId) {
+                        MAP_IDS += resId -> f.getName()
+                        return getString(f.getName())
+                    }
+                } catch {
+                    case e: IllegalArgumentException =>
+                        /*
+                        * Never catch this, we checked the object type before.
+                        */
+                        e.printStackTrace()
+                    case e: IllegalAccessException =>
+                        /*
+                        * Never catch this, since we asked for all *public* fields.
+                        */
+                        e.printStackTrace()
+                }
+            }// if
+        }// for
+
+        null
+    }// getString()
+
+    /**
+     * Gets a formattable string and format it with {@code args}.
+     *
+     * @param resId
+     *            the resource ID.
+     * @param args
+     *            the format arguments.
+     * @return the formatted string.
+     * @throws NullPointerException
+     *             if the resource is not found.
+     */
+    def getString(resId: Int, args: Any*) = getString(resId).format(args : _*)
+
+}
---
+++ code/scala/src/main/scala/group/pals/desktop/app/apksigner/i18n/R.scala
@@ -0,0 +1,171 @@
+/*
+ *    Copyright (C) 2012 Hai Bison
+ *
+ *    See the file LICENSE at the root directory of this project for copying
+ *    permission.
+ */
+
+package group.pals.desktop.app.apksigner.i18n
+
+/**
+ * The resources.
+ *
+ * @author Hai Bison
+ * @since v1.6.5 beta
+ */
+object R {
+
+    /**
+     * The string resources.
+     * <p>
+     * This class is generated automatically, you shouldn't rely on the values
+     * for your works.
+     * </p>
+     *
+     * @author Hai Bison
+     * @since v1.6.5 beta
+     */
+    object string {
+
+        lazy final val about = 0x00000001
+        lazy final val alias = 0x00000002
+        lazy final val alias_name = 0x00000003
+        lazy final val alias_password = 0x00000004
+        lazy final val align = 0x00000005
+        lazy final val aligned = 0x00000006
+        lazy final val apk_aligner_thread = 0x00000007
+        lazy final val apk_alignment = 0x00000008
+        lazy final val apk_alignment_verifier_thread = 0x00000009
+        lazy final val BAD = 0x0000000a
+        lazy final val cancel = 0x0000000b
+        lazy final val cancelled = 0x0000000c
+        lazy final val certificate = 0x0000000d
+        lazy final val certificate_chain_length = 0x0000000e
+        lazy final val certificate_fingerprints = 0x0000000f
+        lazy final val choose = 0x00000010
+        lazy final val choose_directory = 0x00000011
+        lazy final val choose_file = 0x00000012
+        lazy final val choose_jdk_path = 0x00000013
+        lazy final val city_or_locality = 0x00000014
+        lazy final val compressed = 0x00000015
+        lazy final val confirm = 0x00000016
+        lazy final val confirmation = 0x00000017
+        lazy final val context_menu_clear = 0x00000018
+        lazy final val context_menu_clear_and_paste = 0x00000019
+        lazy final val context_menu_copy = 0x0000001a
+        lazy final val context_menu_copy_all = 0x0000001b
+        lazy final val context_menu_cut = 0x0000001c
+        lazy final val context_menu_delete = 0x0000001d
+        lazy final val context_menu_paste = 0x0000001e
+        lazy final val context_menu_select_all = 0x0000001f
+        lazy final val country_code = 0x00000020
+        lazy final val creation_date = 0x00000021
+        lazy final val desc_apk_files = 0x00000022
+        lazy final val desc_jar_files = 0x00000023
+        lazy final val desc_jdk_path = 0x00000024
+        lazy final val desc_keystore_files = 0x00000025
+        lazy final val desc_load_apk_file = 0x00000026
+        lazy final val desc_load_key_file = 0x00000027
+        lazy final val desc_load_target_file = 0x00000028
+        lazy final val desc_save_as = 0x00000029
+        lazy final val desc_zip_files = 0x0000002a
+        lazy final val done = 0x0000002b
+        lazy final val entry_type = 0x0000002c
+        lazy final val error = 0x0000002d
+        lazy final val exit = 0x0000002e
+        lazy final val file = 0x0000002f
+        lazy final val first_and_last_name = 0x00000030
+        lazy final val generate_keyfile = 0x00000031
+        lazy final val help = 0x00000032
+        lazy final val host = 0x00000033
+        lazy final val information = 0x00000034
+        lazy final val internet_connection = 0x00000035
+        lazy final val issuer = 0x00000036
+        lazy final val key_generator = 0x00000037
+        lazy final val key_tools = 0x00000038
+        lazy final val keystore_provider = 0x00000039
+        lazy final val keystore_type = 0x0000003a
+        lazy final val language = 0x0000003b
+        lazy final val list_entries = 0x0000003c
+        lazy final val msg_alias_is_empty = 0x0000003d
+        lazy final val msg_alias_password_is_empty = 0x0000003e
+        lazy final val msg_alias_passwords_dont_match = 0x0000003f
+        lazy final val msg_at_least_one_certificate_issuer_field_is_required = 0x00000040
+        lazy final val msg_cancelled_update = 0x00000041
+        lazy final val msg_cannot_save_a_file_here = 0x00000042
+        lazy final val msg_do_you_want_to_exit = 0x00000043
+        lazy final val msg_file_is_signed = 0x00000044
+        lazy final val msg_html_loading = 0x00000045
+        lazy final val msg_html_loading_fonts = 0x00000046
+        lazy final val msg_html_loading_language = 0x00000047
+        lazy final val msg_html_loading_theme = 0x00000048
+        lazy final val msg_invalid_proxy_host = 0x00000049
+        lazy final val msg_invalid_proxy_port = 0x0000004a
+        lazy final val msg_keyfile_doesnt_exist = 0x0000004b
+        lazy final val msg_keyfile_generated_successfully = 0x0000004c
+        lazy final val msg_load_a_file_to_sign = 0x0000004d
+        lazy final val msg_load_apk_file = 0x0000004e
+        lazy final val msg_local_update_available = 0x0000004f
+        lazy final val msg_password_is_empty = 0x00000050
+        lazy final val msg_passwords_dont_match = 0x00000051
+        lazy final val msg_restart_app_to_apply_new_language = 0x00000052
+        lazy final val msg_specify_target_file = 0x00000053
+        lazy final val msg_update_cancelled = 0x00000054
+        lazy final val msg_update_cancelled_because_wrong_checksum = 0x00000055
+        lazy final val msg_update_finished = 0x00000056
+        lazy final val msg_updating = 0x00000057
+        lazy final val msg_validity_must_be_larger_than_zero = 0x00000058
+        lazy final val no = 0x00000059
+        lazy final val ok = 0x0000005a
+        lazy final val organization = 0x0000005b
+        lazy final val organizational_unit = 0x0000005c
+        lazy final val owner = 0x0000005d
+        lazy final val passed = 0x0000005e
+        lazy final val password = 0x0000005f
+        lazy final val pmsg_file_is_signed_but_cannot_be_renamed_to_new_one = 0x00000060
+        lazy final val pmsg_aligning_apk = 0x00000061
+        lazy final val pmsg_alignment_done = 0x00000062
+        lazy final val pmsg_app_name = 0x00000063
+        lazy final val pmsg_available_space_is_low = 0x00000064
+        lazy final val pmsg_confirm_aligning_file_not_apk = 0x00000065
+        lazy final val pmsg_error_details = 0x00000066
+        lazy final val pmsg_error_generating_keyfile = 0x00000067
+        lazy final val pmsg_error_signing_file = 0x00000068
+        lazy final val pmsg_exception = 0x00000069
+        lazy final val pmsg_file_not_exist = 0x0000006a
+        lazy final val pmsg_local_update_available = 0x0000006b
+        lazy final val pmsg_override_file = 0x0000006c
+        lazy final val pmsg_sensitive_data_encryption = 0x0000006d
+        lazy final val pmsg_there_is_x_service_running = 0x0000006e
+        lazy final val pmsg_there_are_x_services_running = 0x0000006f
+        lazy final val pmsg_update_filesize_exceeds_limit = 0x00000070
+        lazy final val pmsg_update_finished = 0x00000071
+        lazy final val pmsg_updating = 0x00000072
+        lazy final val pmsg_updating_with_percentage = 0x00000073
+        lazy final val pmsg_valid_from_until = 0x00000074
+        lazy final val pmsg_verifying_alignment_of_apk = 0x00000075
+        lazy final val pmsg_your_keystore_contains_x_entries = 0x00000076
+        lazy final val pmsg_your_keystore_contains_x_entry = 0x00000077
+        lazy final val port = 0x00000078
+        lazy final val save_as = 0x00000079
+        lazy final val serial_number = 0x0000007a
+        lazy final val settings = 0x0000007b
+        lazy final val sign = 0x0000007c
+        lazy final val signature_algorithm_name = 0x0000007d
+        lazy final val signer = 0x0000007e
+        lazy final val state_or_province = 0x0000007f
+        lazy final val target_file = 0x00000080
+        lazy final val updater_service = 0x00000081
+        lazy final val use_proxy = 0x00000082
+        lazy final val username = 0x00000083
+        lazy final val validity = 0x00000084
+        lazy final val verification_failed = 0x00000085
+        lazy final val verification_succesful = 0x00000086
+        lazy final val verify = 0x00000087
+        lazy final val version = 0x00000088
+        lazy final val warning = 0x00000089
+        lazy final val yes = 0x0000008a
+
+    }// string
+
+}
